702e3666b3be6e700ee5b34506f7e1317ff3a968
kannapatton <kandirbrts@gmail.com>
2023-05-08 15:30:18 -0500

    organizing

---
+++ problem 3/problem3.py
@@ -0,0 +1,16 @@
+#make a score tracker for two teams apples and bananas
+#first line of input gives the number of 3 point shots for apples
+
+#second line of input gives the number of 2 point shots for apples
+
+#third line of input gives the number of 1 point shots for apples
+
+#fourth line of input gives the number of 3 point shots for bananas
+
+#fifth line of input gives the number of 2 point shots for bananas
+
+#sixth line of input gives the number of 1 point shots for bananas
+
+
+#output should be a for apples if they won, b for bananas if they won, or t if tie
+print('hello')
